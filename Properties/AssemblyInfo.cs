cb8ced5ddd2ee846db1ebf8105a259663796adc5
tman0 <tman0@live.com>
2012-09-09 15:04:37 -0400

    Update assembly information

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("tman0.Launcher")]
+[assembly: AssemblyTitle("MCLauncher by tman0")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("tman0.Launcher")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("MCLauncher")]
+[assembly: AssemblyCopyright("Copyright ©  2012 tman0")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
